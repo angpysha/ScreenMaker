1faa991c5e955c0b72ee758ac0381493ccaf3d63
angpysha <angpysha@outlook.com>
2016-11-23 19:06:18 +0200

    Added keyhook,changed singleton

--- AppLibrary/KeyHook.cpp
+++ AppLibrary/KeyHook.cpp
@@ -1,18 +1,52 @@
 #include "stdafx.h"
 #include "KeyHook.h"
-
-KeyHook* key_hook = new KeyHook;
+#include <algorithm>
+KeyHook *KeyHook::instanse;
 KeyHook::KeyHook()
 {
 	//this->instanse = this;
 	//proc = hookproc;
 }
-
 KeyHook* KeyHook::getInstance()
 {
-	/*if (instanse)
-		return instanse;*/
-	return new KeyHook();
+	if (!instanse)
+		instanse=new KeyHook();
+	return instanse;
+}
+
+void KeyHook::setOptionsEventHendler(std::function<void()> &func)
+{
+	try
+	{
+		this->openParams = func;
+	} catch(std::exception)
+	{
+		return;
+	}
+	//this->openParams
+}
+
+void KeyHook::callOpenParams()
+{
+	if (this->openParams)
+		this->openParams();
+}
+
+void KeyHook::setExitOptionEventHandler(function<void()>& func)
+{
+	try
+	{
+		this->exitOption = func;
+	} catch(std::exception e)
+	{
+		return;
+	}
+}
+
+void KeyHook::callExitApplication()
+{
+	if (this->exitOption)
+		this->exitOption();
 }
 
 
@@ -32,13 +66,61 @@ BOOL KeyHook::setOpenOptionsEventHandler(std::function<void(std::string)> &func)
 }
 LRESULT CALLBACK KeyHook::hookproc_s(int vCode, WPARAM wparam, LPARAM lparam)
 {
-	return key_hook->hookproc(vCode, wparam, lparam);
+	return instanse->hookproc(vCode, wparam, lparam);
 	//return CallNextHookEx(hhook, vCode, wparam, lparam);
 	return LRESULT();
 }
 
 LRESULT KeyHook::hookproc(int vCode, WPARAM wparam, LPARAM lparam)
 {
+	if (vCode >=0)
+	{
+		switch (wparam)
+		{
+		case (WM_KEYDOWN):
+			{
+				auto p = reinterpret_cast<PKBDLLHOOKSTRUCT>(lparam);
+				for (auto z : this->keys)
+				{
+					//vector<bool> tmp;
+					if (std::find(z.second.begin() + 1, z.second.end(), p->vkCode)!=z.second.end())
+						//tmp.push_back(true);
+					{
+						trig[z.first].push_back(true);
+					}
+					//if (std::all_of(tmp.begin()+1,tmp.end(),TRUE))
+				}
+
+				for (auto kk : this->trig)
+				{
+					if (kk.second.size() == this->keys[kk.first].size())
+					{
+						switch (kk.first)
+						{
+						case (Action::Exit):
+							{
+							this->callExitApplication();
+						}; break;
+						case (Action::Options):
+							{
+							this->callOpenParams();
+							//Maybe reload settings if changed
+						}; break;
+						case (Action::Screen):
+							{
+								
+						};break;
+						default:break;
+						}
+					}
+				}
+		}; break;
+		case (WM_KEYUP):
+			{
+			auto p = reinterpret_cast<PKBDLLHOOKSTRUCT>(lparam);
+		}; break;
+		}
+	}
 	return CallNextHookEx(hhook, vCode, wparam, lparam);
 	return LRESULT();
 }
--- AppLibrary/KeyHook.h
+++ AppLibrary/KeyHook.h
@@ -2,21 +2,30 @@
 #include <functional>
 #include "types.h"
 #include <Windows.h>
-
+#include "KeyCodeWrapper.h"
+using namespace std;
 class KeyHook
 {
 public:
-	KeyHook();
-	~KeyHook();
 	BOOL setOpenOptionsEventHandler(std::function<void(std::string)>& func);
 	static LRESULT CALLBACK hookproc_s(int vCode, WPARAM wparam, LPARAM lparam);
 	LRESULT CALLBACK hookproc(int vCode, WPARAM wparam, LPARAM lparam);
 	static KeyHook *getInstance();
+	void setOptionsEventHendler(std::function<void()> &func);
+	void callOpenParams();
+	void setExitOptionEventHandler(function<void()> &func);
+	void callExitApplication();
 	void setHook();
 protected:
 	std::function<void(std::string)> OpenOptions;
+	static KeyHook* instanse;
+	map<Action, vector<uint>> keys;
+	map<Action, vector<bool>> trig;
+	function<void()> openParams;
+	function<void()> exitOption;
 	HOOKPROC proc;
 	 HHOOK hhook;
-	 static KeyHook *instanse;
+	 KeyHook();
+	 virtual ~KeyHook();
  };
 
